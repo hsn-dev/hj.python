928b4d891bb9ccd0eaba8f68fbcd468eab3f6d95
Hassan Farooq <hsnfrq.47@gmail.com>
2020-04-05 05:40:57 -0700

    error fixed in mutable file

--- Learn Python/mutables_vs_immutables.py
+++ Learn Python/mutables_vs_immutables.py
@@ -1,13 +1,21 @@
+'''
+A mutable object can be changed after it is created, and an immutable object can't.
 
+The key thing to note is that the bindings are unchangeable, not the objects they are bound to.
+Let us consider a tuple t = (‘holberton’, [1, 2, 3])
 
-# MUTABLES
+This is a subtle point, but nonetheless important: the “value” of an immutable object can’t change,
+ but it’s constituent objects can.
+'''
+
+# IMMUTABLES
 a = "hassan"
 print(f'Addres of a: {id(a)}')
 
 a = "farooq"
 print(f'Addres of a: {id(a)}')
 
-# IMMUTABLES
+# MUTABLES
 l = [1,2,3,4,5]
 print(l)
 print(f'Addres of list: {id(l)}')
